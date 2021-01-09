b12af4553bc661890127d785820ceca1517f33c7
Omar Abel Rodríguez-López <oarodriguez.mx@gmail.com>
2021-01-08 22:40:43 -0600

    Calculate the free energy from the sparse transfer matrix

--- src/spinchains_manybody/ising.py
+++ src/spinchains_manybody/ising.py
@@ -4,6 +4,8 @@ from math import log
 import numpy as np
 from numba import njit
 from scipy.linalg import eigvals
+from scipy.sparse import csr_matrix
+from scipy.sparse.linalg import eigs as sparse_eigs
 
 from .utils import bin_digits
 
@@ -63,6 +65,73 @@ def dense_transfer_matrix(temp: float,
     return w_log_matrix
 
 
+@njit
+def _csr_log_transfer_matrix_parts(temp: float,
+                                   mag_field: float,
+                                   hop_params_list: np.ndarray,
+                                   spin_proj_table: np.ndarray):
+    """Calculate the parts of the sparse transfer matrix.
+
+    We use numba to accelerate the calculations.
+    """
+    num_rows, num_neighbors = spin_proj_table.shape
+    # Use lists, since we do not know a priori how many nonzero elements
+    # the transfer matrix has.
+    _nnz_elems = []
+    _nnz_rows = []
+    _nnz_cols = []
+
+    # Start loop.
+    for idx in range(num_rows):
+        for jdx in range(num_rows):
+            is_nonzero = True
+            for sigma in range(1, num_neighbors):
+                proj_one = spin_proj_table[idx, sigma]
+                proj_two = spin_proj_table[jdx, sigma - 1]
+                is_nonzero = is_nonzero and (proj_one == proj_two)
+
+            # Cycle to the next index.
+            if not is_nonzero:
+                continue
+
+            proj_one = spin_proj_table[idx, 0]
+            w_elem = (mag_field * proj_one / temp)
+            for edx in range(num_neighbors):
+                hop_param = hop_params_list[edx]
+                proj_two = spin_proj_table[jdx, edx]
+                w_elem += (hop_param * proj_one * proj_two / temp)
+
+            # Store the matrix element.
+            _nnz_elems.append(w_elem)
+            _nnz_rows.append(idx)
+            _nnz_cols.append(jdx)
+
+    nnz_elems = np.asarray(_nnz_elems, dtype=np.float64)
+    nnz_rows = np.asarray(_nnz_rows, dtype=np.int32)
+    nnz_cols = np.asarray(_nnz_cols, dtype=np.int32)
+    return nnz_elems, nnz_rows, nnz_cols
+
+
+def norm_sparse_log_transfer_matrix(temp: float,
+                                    mag_field: float,
+                                    hop_params_list: np.ndarray,
+                                    spin_proj_table: np.ndarray):
+    """Calculate the (sparse) normalized transfer matrix."""
+    num_rows, _ = spin_proj_table.shape
+    nnz_elems, nnz_rows, nnz_cols = \
+        _csr_log_transfer_matrix_parts(temp,
+                                       mag_field,
+                                       hop_params_list,
+                                       spin_proj_table)
+
+    # Normalize matrix elements.
+    max_w_log_elem = np.max(nnz_elems)
+    nnz_elems -= max_w_log_elem
+    w_shape = (num_rows, num_rows)
+    return csr_matrix((nnz_elems, (nnz_rows, nnz_cols)),
+                      shape=w_shape)
+
+
 def get_energy_data(temp: float,
                     mag_field: float,
                     num_spins: int,
@@ -83,3 +152,32 @@ def get_energy_data(temp: float,
     helm_free_erg = -temp * (log(z_aux) / num_spins + max_w_log_elem)
     helm_free_erg_tl = -temp * (log(max_eigvals) + max_w_log_elem)
     return EnergyData(helm_free_erg, helm_free_erg_tl)
+
+
+def energy_thermo_limit(temp: float,
+                        mag_field: float,
+                        hop_params_list: np.ndarray,
+                        spin_proj_table: np.ndarray):
+    """Calculate the Helmholtz free energy of the system."""
+    num_rows, _ = spin_proj_table.shape
+    nnz_elems, nnz_rows, nnz_cols = \
+        _csr_log_transfer_matrix_parts(temp,
+                                       mag_field,
+                                       hop_params_list,
+                                       spin_proj_table)
+
+    # Normalize nonzero matrix elements.
+    max_w_log_elem = np.max(nnz_elems)
+    nnz_elems -= max_w_log_elem
+    norm_nnz_elems = np.exp(nnz_elems)
+    # Construct the sparse matrix.
+    w_shape = (num_rows, num_rows)
+    w_matrix = csr_matrix((norm_nnz_elems, (nnz_rows, nnz_cols)),
+                          shape=w_shape)
+    # Evaluate the largest eigenvalue, since it defines the free energy in
+    # the thermodynamic limit.
+    # noinspection PyTypeChecker
+    w_norm_eigvals, _ = sparse_eigs(w_matrix, k=1, which="LM")
+    max_eigvals = w_norm_eigvals.real[0]
+    helm_free_erg_tl = -temp * (log(max_eigvals) + max_w_log_elem)
+    return helm_free_erg_tl
