0afdfcb3778980dae21d71b7d9c77a0f7eea8a0e
Omar Abel Rodríguez-López <oarodriguez.mx@gmail.com>
2021-10-22 01:25:26 -0500

    Agrega scripts para instalar `isingchat` en ambientes de `conda`

---
+++ scripts/miztli-admin/conda-build.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+#BSUB -q q_expl
+#BSUB -n 4
+#BSUB -m mn461
+#BSUB -R "affinity[core(1):cpubind=numa:membind=localprefer:distribute=pack]"
+#BSUB -oo conda-build_output-job#%J.txt
+#BSUB -eo conda-build_error-job#%J.txt
+
+# Execute conda build in the node to pack isingchat as a conda package.
+
+# Load system modules
+module load SO/ambiente-7
+#module load miniconda/4.10.3
+
+# NOTE: We can retrieve the number of processors (slots) allocated to the job through the environment variable LSB_DJOB_NUMPROC.
+# See https://www.ibm.com/docs/en/spectrum-lsf/10.1.0?topic=variables-environment-variable-reference.
+
+# We have to source our ~/.bashrc file since conda activate command
+# relies on some instructions written in this file.
+source ~/.bashrc
+
+echo "================================"
+echo "Show conda information"
+conda info -a
+echo "--------------------------------"
+
+echo "================================"
+echo "System PATH"
+echo "$PATH"
+echo "--------------------------------"
+
+echo "================================"
+echo "Conda build information"
+conda build --help
+echo "--------------------------------"
+
+echo "================================"
+echo "Conda build information"
+conda build ./conda/recipe -c conda-forge
+echo "--------------------------------"
+
+echo "All tasks finished"
